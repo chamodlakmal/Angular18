1d17c226a77cc4120eec6188543de048a1d189cb
chamodlakmal <chamod.lakmal@pickme.lk>
2025-01-17 10:57:56 +0530

    bootstrap init

--- bootstrap/README.md
+++ bootstrap/README.md
@@ -1,4 +1,4 @@
-# Boostrap
+# Bootstrap
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.10.
 
--- td-forms/src/app/app.component.spec.ts
+++ td-forms/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'boostrap' title`, () => {
+  it(`should have the 'td-forms' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('boostrap');
+    expect(app.title).toEqual('td-forms');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, boostrap');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, td-forms');
   });
 });
