2864a8ecf7eee096123fd7d827ce5cfaa1770444
chamodlakmal <chamod.lakmal@pickme.lk>
2025-03-15 21:04:03 +0530

    preloading strategy added

--- routing-app/src/app/app.config.ts
+++ routing-app/src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, TitleStrategy, withComponentInputBinding, withRouterConfig } from '@angular/router';
+import { PreloadAllModules, provideRouter, TitleStrategy, withComponentInputBinding, withPreloading, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
 import { TemplatePageTitleStrategyService } from './services/template-page-title-strategy.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes,
-    withComponentInputBinding(), withRouterConfig({ paramsInheritanceStrategy: 'always' })),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(
+    routes,
+    withPreloading(PreloadAllModules),
+    withComponentInputBinding(),
+    withRouterConfig({ paramsInheritanceStrategy: 'always' })),
   { provide: TitleStrategy, useClass: TemplatePageTitleStrategyService }],
 };
