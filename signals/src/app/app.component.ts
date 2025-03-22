d2025381c48c0c3aba4d8b05535185caf2f7635d
chamodlakmal <chamod.lakmal@pickme.lk>
2025-03-22 22:28:13 +0530

    view queries added

--- signals/src/app/app.component.ts
+++ signals/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, ElementRef, signal, viewChild, viewChildren } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from "./counter/counter.component";
 import { EffectsComponent } from "./effects/effects.component";
@@ -9,11 +9,12 @@ import { RxjsInteropComponent } from "./rxjs-interop/rxjs-interop.component";
 import { ToObservableComponent } from "./to-observable/to-observable.component";
 import { SignalInputsComponent } from "./signal-inputs/signal-inputs.component";
 import { ModelInputsComponent } from "./model-inputs/model-inputs.component";
+import { ViewQueriesComponent } from "./view-queries/view-queries.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CounterComponent, EffectsComponent, EqualityComponent, UntrackedComponent, CleanupComponent, RxjsInteropComponent, ToObservableComponent, SignalInputsComponent, ModelInputsComponent],
+  imports: [RouterOutlet, CounterComponent, EffectsComponent, EqualityComponent, UntrackedComponent, CleanupComponent, RxjsInteropComponent, ToObservableComponent, SignalInputsComponent, ModelInputsComponent, ViewQueriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -25,11 +26,25 @@ export class AppComponent {
   //    this.showCleanUpComponent.set(!this.showCleanUpComponent());
   //  }
 
-  parentChecked = true
+  //parentChecked = true
 
   //handleCheckedChange(value: boolean) {
   //  console.log("parent handle checked Change : ", value)
   //  this.parentChecked = value
   //}
+
+  cmp = viewChild(ViewQueriesComponent, { read: ElementRef })
+  el = viewChild<ElementRef>('el')
+
+  elementSize = computed(() => {
+    const nativeElement = this.el()?.nativeElement
+    return nativeElement ? nativeElement.offsetWidth * nativeElement.offsetHeight : 0
+  })
+
+  constructor() {
+    effect(() => {
+      console.log("cmp : ", this.cmp(), " el : ", this.el());
+    })
+  }
 
 }
---
+++ signals/src/app/view-queries/view-queries.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-queries',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-queries.component.html',
+  styleUrl: './view-queries.component.css'
+})
+export class ViewQueriesComponent {
+  age = 45;
+}
