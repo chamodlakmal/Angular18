52a89858af82d97cad6f143dae8cc885fd98e649
chamodlakmal <chamod.lakmal@pickme.lk>
2025-03-27 15:46:34 +0530

    unit tests added

--- http-client/src/app/data-fetcher/data-fetcher.component.spec.ts
+++ http-client/src/app/data-fetcher/data-fetcher.component.spec.ts
@@ -2,22 +2,35 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DataFetcherComponent } from './data-fetcher.component';
 
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+
 describe('DataFetcherComponent', () => {
   let component: DataFetcherComponent;
   let fixture: ComponentFixture<DataFetcherComponent>;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DataFetcherComponent]
+      imports: [DataFetcherComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(DataFetcherComponent);
+    httpTestingController = TestBed.inject(HttpTestingController);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should make a call and update the data', () => {
+    const mockresponse = { data: 'mock data' };
+    component.getJsonData();
+
+    const req = httpTestingController.expectOne('https://jsonplaceholder.typicode.com/users/1');
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockresponse);
+
+    expect(component.jsonData).toEqual(mockresponse);
+  })
 });
