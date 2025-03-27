dd06516e7d2261c620ee1e2fbfd7b4d8659f1d33
chamodlakmal <chamod.lakmal@pickme.lk>
2025-03-27 19:59:31 +0530

    error handler added

--- http-client/src/app/app.component.ts
+++ http-client/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { PostListComponent } from "./post-list/post-list.component";
 import { DataFetcherComponent } from "./data-fetcher/data-fetcher.component";
 import { QueryParamsComponent } from "./query-params/query-params.component";
 import { FileUploaderComponent } from "./file-uploader/file-uploader.component";
+import { ErrorHandlerComponent } from "./error-handler/error-handler.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PostListComponent, DataFetcherComponent, QueryParamsComponent, FileUploaderComponent],
+  imports: [RouterOutlet, PostListComponent, DataFetcherComponent, QueryParamsComponent, FileUploaderComponent, ErrorHandlerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ http-client/src/app/error-handler/error-handler.component.ts
@@ -0,0 +1,55 @@
+import { JsonPipe } from '@angular/common';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import e from 'cors';
+import { catchError, retry, throwError, timer } from 'rxjs';
+
+@Component({
+  selector: 'app-error-handler',
+  standalone: true,
+  imports: [JsonPipe],
+  templateUrl: './error-handler.component.html',
+  styleUrl: './error-handler.component.css'
+})
+export class ErrorHandlerComponent {
+
+  http = inject(HttpClient)
+  data: any
+  error: any
+
+  fetchData() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts/1').pipe(retry({
+      count: 3,
+      delay: (error, count) => {
+        console.log(`Tried ${count} times`);
+
+        if (error.status === 404) {
+          throw error
+        } else {
+          return timer(2000)
+        }
+      }
+    }), catchError(this.handleError.bind(this))).subscribe({
+      next: data => {
+        console.log(data)
+        this.data = data
+      },
+      error: error => {
+        console.error(error)
+      }
+    });
+  }
+
+  handleError(error: HttpErrorResponse) {
+    console.log(error);
+
+    if (error.status === 0) {
+      this.error = `No network error. Pls check the connection`
+    } else {
+      this.error = `Backend returned error code ${error.status}. ${error.message}`
+    }
+
+    return throwError(() => 'Something bad happened; please try again later.');
+  }
+
+}
